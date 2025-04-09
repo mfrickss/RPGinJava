759a90b8a9c73e0dd5a9a2f93ba5241215ee3e03
mfrickss <mfricks777@gmail.com>
2025-04-08 21:01:20 -0300

    opções

--- RPG/src/Main.java
+++ RPG/src/Main.java
@@ -1,10 +1,29 @@
 import Monstros.*;  // Importa tudo dentro da pasta Monstros
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         Random random = new Random();
         Monstro[] monstros = new Monstro[3];
+        int choice;
+
+        System.out.println("Você acorda em uma masmorra, não há memórias em sua mente, você não sabe como ou porquê está ali.\nAo olhar ao redor, você vê 3 caminhos: ");
+
+        System.out.println("1. Uma portão que leva à um destino oculto pela escuridão! ");
+        System.out.println("2. Uma abertura na parede, parece um buraco causado por algo grande!");
+        System.out.println("3. Uma porta de ferro que parece muito pesada, é impossível saber o que tem atrás.");
+        linha();
+        System.out.print("Qual sua escolha?(1-3): ");
+        choice = scanner.nextInt();
+
+        if(choice == 1){
+            
+        }
+
+
+
 
 
         for (int i = 0; i < monstros.length; i++) {
@@ -24,5 +43,9 @@ public class Main {
             monstros[i].printStats();
             System.out.println();
         }
+    }
+
+     static void linha(){
+        System.out.println("===================================");
     }
 }
